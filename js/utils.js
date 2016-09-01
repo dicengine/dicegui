32b402c2b9ec41897946e63f0a1ed4f3d461d779
Dan Turner <dzturne@sandia.gov>
2016-09-01 06:06:39 -0600

    Implemented an alternative to the bootstrap drop down lists
    This one allows for one of the elements to fill the leftover space
    in the column

--- js/utils.js
+++ js/utils.js
@@ -3,3 +3,59 @@ document.getElementById("clearConsoleIcon").onclick = function() {eraseText("con
 function eraseText(object_id) {
     document.getElementById(object_id).innerHTML = "Console output:" + '<br/><br/>';
 }
+
+$('#viewerControlPanel').on('shown.bs.collapse', function () {
+    resizeView();
+})
+$('#viewerControlPanel').on('hidden.bs.collapse', function () {
+    resizeView();
+})
+$('#consolePanel').on('shown.bs.collapse', function () {
+    resizeView();
+})
+$('#consolePanel').on('hidden.bs.collapse', function () {
+    resizeView();
+})
+
+function resizeView() {
+    var viewerControlHeight = document.getElementById("viewerControlPanel").offsetHeight;
+    var consoleHeight = document.getElementById("consolePanel").offsetHeight;
+    var totalHeight = document.getElementById("innerFluidLeftCol").clientHeight;
+    console.log(totalHeight);
+    console.log(viewerControlHeight);
+    console.log(consoleHeight);
+    document.getElementById("viewerPanel").style.height = totalHeight - viewerControlHeight - consoleHeight - 20 + "px";
+}
+
+
+$(".inner-fluid-left-col > div").click(function(){
+    // if you need element's ID
+    var divID = this.id;
+    alert("I Clicked this id: " + divID);
+});
+
+// toggle boxes up and down
+$(".toggler").click(function(){
+    var H = $(this).parent().parent('div').parent('div').outerHeight();
+    var bannerH = $(this).parent().parent('div').outerHeight();
+    if(H==bannerH){
+        $(this).parent().parent('div').parent('div').outerHeight("auto");
+    }else{
+        $(this).parent().parent('div').parent('div').outerHeight(bannerH + "px");
+    }
+    // now find any fill divs and resize them
+    // total height of all divs in the inner fluid right col
+    var sumHeight = 0;
+    $(".inner-fluid-right-col > div").each(function() {
+        sumHeight += $(this).outerHeight();
+    });
+    //alert("sum: " + sumHeight + 'px');
+    var currentFillHeight = $('.fill-div').outerHeight();
+    //alert("fill current: " + currentFillHeight);
+    sumHeight -= currentFillHeight;
+    var totalHeight = $('.inner-fluid-right-col').outerHeight();
+    //alert("total: " + totalHeight);
+    var resizeHeight = totalHeight - sumHeight - 20;
+    $('.fill-div').outerHeight(resizeHeight);
+    
+});
