2bd0c0551f4ac07ca9b7fad114598617e8f6c83c
Dan Turner <dzturne@sandia.gov>
2016-09-02 12:29:46 -0600

    Moved image viewer to sub fill div

--- js/utils.js
+++ js/utils.js
@@ -1,20 +1,15 @@
 $(window).load(function(){
-    // TODO  set the default show/hide positions for toggles
-    
+    // TODO  set the default show/hide positions for toggles    
     // resize the full div elements
     resizeAll();
 });
 
+// launch external links in the default browser not the frame
 const shell = require('electron').shell;
-
 $(document).on('click', 'a[href^="http"]', function(event) {
     event.preventDefault();
     shell.openExternal(this.href);
 });
-
-window.addEventListener('resize', function(){resizeAll();}, true);
-
-document.getElementById("clearConsoleIcon").onclick = function() {eraseText("consoleWindow")};
 
 // toggle the params menu on or off
 document.getElementById("paramsButton").onclick = function(){
@@ -30,10 +25,19 @@ document.getElementById("paramsButton").onclick = function(){
     resizeAll();
 };
 
+// clear the console text
+document.getElementById("clearConsoleIcon").onclick = function() {eraseText("consoleWindow")};
 function eraseText(object_id) {
     document.getElementById(object_id).innerHTML = "Console output:" + '<br/><br/>';
 }
 
+// resize the full divs on window resize
+window.addEventListener('resize', function(){resizeAll();}, true);
+
+// toggle boxes up and down
+$(".toggler").click(function(){resizeView(this);});
+
+// resize the divs to fill the column when called from a toggle button
 function resizeView(toggler) {
     var H = $(toggler).parent().parent('div').parent('div').outerHeight();
     var bannerH = $(toggler).parent().parent('div').outerHeight();
@@ -47,14 +51,18 @@ function resizeView(toggler) {
         $(toggler).parent().parent('div').css('border-radius','5px');
     }
     // now find any fill divs and resize the
+    // NOTE assumes that top level div has an id (TODO address this)
     resizeFullDivs("#" + $(toggler).parent().parent('div').parent('div').parent('div').attr('id'));    
 }
 
+// resize all columns and inner-columns to make fill-divs fill the column
 function resizeAll(){
     resizeFullDivs("#innerFluidLeftCol");
     resizeFullDivs("#innerFluidRightCol");
+    resizeFullDivs("#fillDivLeft");
 }
 
+// resize the elements within the target div
 function resizeFullDivs(targetDiv){
     // total height of all divs in the inner fluid right col
     var sumHeight = 0;
@@ -71,5 +79,3 @@ function resizeFullDivs(targetDiv){
     })
 }
 
-// toggle boxes up and down
-$(".toggler").click(function(){resizeView(this);});
