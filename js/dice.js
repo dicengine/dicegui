9e2d331b74b4f7371afc0f4f2152c1ac148f8b11
Dan Turner <dzturne@sandia.gov>
2016-09-12 09:48:56 -0600

    added params file writer to js/dice.js

--- js/dice.js
+++ js/dice.js
@@ -1,5 +1,6 @@
 document.getElementById("runLi").onclick = function() {
     writeInputFile();
+    writeParamsFile();
     callDICeExec();
 };
 
@@ -27,8 +28,6 @@ function callDICeExec() {
         objDiv.scrollTop = objDiv.scrollHeight;
     });    
 }
-
-
 
 function writeInputFile() {
     fileName = workingDirectory;
@@ -69,3 +68,58 @@ function writeInputFile() {
         $('#consoleWindow').append('input.xml file has been successfully saved <br/>');
     });
 }
+
+function writeParamsFile() {
+    paramsFile = workingDirectory;
+    if(os.platform()=='win32'){
+        paramsFile += '\\params.xml';
+    }else{
+        paramsFile += '/params.xml';
+    }
+    $('#consoleWindow').append('writing parameters file ' + paramsFile + '<br/>');
+    var content = '';
+    content += '<!-- Auto generated parameters file from DICe GUI -->\n';
+    content += '<ParameterList>\n';
+    content += '<Parameter name="interpolation_method" type="string" value="KEYS_FOURTH" />\n';
+    content += '<Parameter name="optimization_method" type="string" value="GRADIENT_BASED" />\n';
+    content += '<Parameter name="initialization_method" type="string" value="USE_FIELD_VALUES" />\n';
+    if($("#translationCheck")[0].checked){
+        content += '<Parameter name="enable_translation" type="bool" value="true" />\n';
+    }else{
+        content += '<Parameter name="enable_translation" type="bool" value="false" />\n';
+    }
+    if($("#rotationCheck")[0].checked){
+        content += '<Parameter name="enable_rotation" type="bool" value="true" />\n';
+    }else{
+        content += '<Parameter name="enable_rotation" type="bool" value="false" />\n';
+    }
+    if($("#normalStrainCheck")[0].checked){
+        content += '<Parameter name="enable_normal_strain" type="bool" value="true" />\n';
+    }else{
+        content += '<Parameter name="enable_normal_strain" type="bool" value="false" />\n';
+    }
+    if($("#shearStrainCheck")[0].checked){
+        content += '<Parameter name="enable_shear_strain" type="bool" value="true" />\n';
+    }else{
+        content += '<Parameter name="enable_shear_strain" type="bool" value="false" />\n';
+    }
+    content += '<Parameter name="output_delimiter" type="string" value="," />\n'
+    content += '<ParameterList name="output_spec"> \n';
+    content += '<Parameter name="COORDINATE_X" type="bool" value="true" />\n';
+    content += '<Parameter name="COORDINATE_Y" type="bool" value="true" />\n';
+    content += '<Parameter name="DISPLACEMENT_X" type="bool" value="true" />\n';
+    content += '<Parameter name="DISPLACEMENT_Y" type="bool" value="true" />\n';
+    content += '<Parameter name="SIGMA" type="bool" value="true" />\n';
+    content += '<Parameter name="GAMMA" type="bool" value="true" />\n';
+    content += '<Parameter name="BETA" type="bool" value="true" />\n';
+    content += '<Parameter name="STATUS_FLAG" type="bool" value="true" />\n';
+    content += '</ParameterList>\n';
+    content += '</ParameterList>\n';
+    fs.writeFile(paramsFile, content, function (err) {
+        if(err){
+            alert("ERROR: an error ocurred creating the file "+ err.message)
+         }
+        $('#consoleWindow').append('params.xml file has been successfully saved <br/>');
+    });
+}
+
