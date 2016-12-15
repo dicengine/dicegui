33d051c3a730c26dcb4a152b2c8f0853d4cc1690
Dan Turner <dzturne@sandia.gov>
2016-12-15 07:59:44 -0700

    maximize window at startup

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({width: 800, height: 600})
 
+    win.maximize()
+    
     // and load the index.html of the app.
     win.loadURL(`file://${__dirname}/index.html`)
 
