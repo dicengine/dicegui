12d9b8317f7e636a01816d99b548d445a784dc60
Dan Turner <dzturne@sandia.gov>
2020-11-25 07:54:34 -0700

    plotly panzoom swap commit 3
    testing if it's possible to plot contours over an image

--- js/preview.js
+++ js/preview.js
@@ -49,11 +49,11 @@ function updatePreview(file_obj){
 }
 
 
-// test to print out the shapes in the image every 10 sec
-window.setInterval(function(){
-    var divID = "plotlyDiv";
-    console.log(document.getElementById(divID).layout.shapes);
-}, 10000);
+//// test to print out the shapes in the image every 10 sec
+//window.setInterval(function(){
+//    var divID = "plotlyDiv";
+//    console.log(document.getElementById(divID).layout.shapes);
+//}, 10000);
 
 function updateImage(imageSpec){
     console.log('updateImage(): image path ' + imageSpec.displayPath);
@@ -103,6 +103,21 @@ function updateImage(imageSpec){
     if( $('#plotlyViewerLeft').is(':empty') ) {
         $("#plotlyViewerLeft").append('<div id="' + divID + '" class="plot_div" style="height:100%; width:100%; float:left;" ></div>');
     }
+    
+    var data = [ {
+//        z: [0, 0, 1, 1],
+//        x: [50, 100, 100, 0],
+//        y: [0, 0, 100, 100],
+        z: [0.65,0.6,0.7,0.6,0.72,0.72,1.0,0.7,0.5],
+        x: [180,230,360,100,180,300,350,45,110],
+        y: [510,100,515,300,760,600,900,810,80],
+        type: 'contour',
+        opacity: 0.6,
+        colorscale: 'Jet',
+        showscale: true,
+        autocontour: true,
+      }];
+    
     // TODO call restyle or relayout instead of newPlot each time?
-    Plotly.newPlot(document.getElementById(divID),[],layout,config);
+    Plotly.newPlot(document.getElementById(divID),data,layout,config);
 }
